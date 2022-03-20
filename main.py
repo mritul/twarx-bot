a1817155d14262f2dae76e595dac3e09e59d115d
mritul21 <mritul2003@gmail.com>
2022-03-20 07:28:13 +0530

    Now the bot responds only if .twhelp or .twfetch are called

--- main.py
+++ main.py
@@ -18,27 +18,30 @@ class MyClient(discord.Client):
         if str(content)==".twhelp":
             await message.channel.send("Use the command `.twfetch <<like/retweet>> <<username(without @)>>`")
             return
-        if len(content.split())!=3:
-            await message.channel.send(f"Hey there...{author} sent a message that reads {content}. Hit `.twhelp` for valid commands")        
-            return
-        elif (len(content.split())==3 and content.split()[0]!=".twfetch" ):
-            await message.channel.send(f"Hey there...{author} sent a message that reads {content}. Hit `.twhelp` for valid commands")
-            return
-        elif(len(content.split())==3 and content.split()[0]==".twfetch" and (content.split()[1] not in ["like","retweet"])):
-            await message.channel.send(f"Syntax error type in `.twhelp` for syntax")
-            return
-        elif (len(content.split())==3 and content.split()[0]==".twfetch" and (content.split()[1]=="like" or content.split()[1]=="retweet")):
-            graphOutput = graph.plotGraphs(content.split()[2])
-            if graphOutput=="User not found":
-                await message.channel.send("User not found")
-            else:
-                await message.channel.send(f"Here is your requested graph {author}")
-                if(content.split()[1]=="like"):
-                    await message.channel.send(file = discord.File("./likes.jpeg"))
-                    return
+        if content.split()[0]==".twfetch":
+            if len(content.split())!=3:
+                await message.channel.send(f"Hey there...{author} sent a message that reads {content}. Hit `.twhelp` for valid commands")        
+                return
+            elif (len(content.split())==3 and content.split()[0]!=".twfetch" ):
+                await message.channel.send(f"Hey there...{author} sent a message that reads {content}. Hit `.twhelp` for valid commands")
+                return
+            elif(len(content.split())==3 and content.split()[0]==".twfetch" and (content.split()[1] not in ["like","retweet"])):
+                await message.channel.send(f"Syntax error type in `.twhelp` for syntax")
+                return
+            elif (len(content.split())==3 and content.split()[0]==".twfetch" and (content.split()[1]=="like" or content.split()[1]=="retweet")):
+                graphOutput = graph.plotGraphs(content.split()[2])
+                if graphOutput=="User not found":
+                    await message.channel.send("User not found")
                 else:
-                    await message.channel.send(file = discord.File("./retweets.jpeg"))
-                    return
+                    await message.channel.send(f"Here is your requested graph {author}")
+                    if(content.split()[1]=="like"):
+                        await message.channel.send(file = discord.File("./likes.jpeg"))
+                        return
+                    else:
+                        await message.channel.send(file = discord.File("./retweets.jpeg"))
+                        return
+        else:
+            pass
 
 client = MyClient()
 client.run(TOKEN)
